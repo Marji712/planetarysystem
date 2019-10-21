911d365fee6fb39965b3d19891ed45934c1b4cc8
Marji Symonds <symondsm@oregonstate.edu>
2019-10-20 17:25:37 -0700

    Print lines added

---
+++ planetarysystem.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Welcome to the Solar System!")
+	fmt.Println("There are 9 planets to explore.")
+	fmt.Println("What is your name?")
+	fmt.Println("Nice to meet you, . My name is Eliza, I'm an old friend of Alexa")
+	fmt.Println("Let's go on an adventure!")
+	fmt.Println("Shall I randomly choose a planet for you to vist? (Y or N)")
+	fmt.Println("Name the planet you would like to visit")
+	fmt.Println("Traveling to  ...")
+	fmt.Println("Arrived a .  ")
+
+}
