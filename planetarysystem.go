e33674d3372dc85c5a8d69dfe42e11239501262a
Marji Symonds <symondsm@oregonstate.edu>
2019-10-22 11:44:10 -0700

    Reading in json

--- planetarysystem.go
+++ planetarysystem.go
@@ -5,9 +5,13 @@ import (
 	"bufio"
 	"os"
 	"strings"
+	"encoding/json"
+	"io/ioutil"
 )
 
 func main() {
+	jsonread()
+
 	fmt.Println("Welcome to the Solar System!")
 	fmt.Println("There are 9 planets to explore.")
 	reader := bufio.NewReader(os.Stdin)
@@ -15,7 +19,11 @@ func main() {
 	name, _ := reader.ReadString('\n')
 	fmt.Println("Nice to meet you,",name, ". My name is Eliza, I'm an old friend of Alexa")
 	fmt.Println("Let's go on an adventure!")
-	pickchoice()
+
+	pickchoice()			//function to take input or random planet
+
+						//close json file
+
 	fmt.Println("Traveling to  ...")
 	fmt.Println("Arrived a .  ")
 
@@ -32,4 +40,16 @@ func pickchoice() {
 	} else {
 		pickchoice()
 	}
+}
+
+func jsonread() {
+	jsonFile, err := ioutil.ReadFile(strings.ToLower(os.Args[0]))	
+	if err != nil {								//check if there was an error
+		fmt.Println(err)
+		return
+	}
+	var planets map[string]interface{}
+	json.Unmarshal([]byte(jsonFile), &planets)
+
+	fmt.Println(planets["Mars"])
 }
